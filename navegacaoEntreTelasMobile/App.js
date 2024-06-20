462804142d58b616b3c4ba6712d7363488be186b
alee <penzalessandra1@gmail.com>
2024-06-20 11:01:31 -0300

    Questão 1
    Criação dos botões na tela de inicio

---
+++ navegacaoEntreTelasMobile/App.js
@@ -0,0 +1,28 @@
+import {View, Text , StyleSheet, Button} from "react-native";
+import { NavigationContainer } from "@react-navigation/native"; 
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const PilhasTelas = createNativeStackNavigator()
+
+function TelaInicio( {route, navigation}){
+    return(
+        <View style={styles.container}>
+            <Text style={styles.titulo}>Selecione um botão: </Text>
+            <Button 
+                title="Detalhes do Campus IFC"
+                color="#07da5c"
+                onPress={function(){
+                    navigation.navigate("OutraTela")
+                }}
+            ></Button>
+            <Button 
+                title="Página de cursos"
+                color="#07da5c"
+                onPress={function(){
+                    navigation.navigate("SelecionarCurso")
+                }}
+            ></Button>
+
+        </View>
+    )
+}
